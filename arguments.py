4c524113431a6d8125b94b321c7893569c0a2b2a
iloveyumyumshrimpnoodles <125209101+iloveyumyumshrimpnoodles@users.noreply.github.com>
2023-04-23 02:20:39 +0200

    update
    - Improved readability
    - Fixed some left-open file descriptors
    - Added license header to file
    - Added typing

--- arguments.py
+++ arguments.py
@@ -1,101 +1,136 @@
+"""
+Generate a Waifu on a wallpaper!
+
+Copyright (C) 2023 Momijie
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import argparse
+
 class Arguments:
     def __init__(self):
-        self.prog_name = 'wallpaper-gen'
-        self.description = 'Creeates a kawaii-waifu wallpaper!'
-        self.version = '0.1.0'
+        self.prog_name = "wallpaper-gen"
+        self.description = "Generate a Waifu on a wallpaper!"
+        self.version = "0.1.0"
         self.arguments = [
-                (('filename'), {
-                    'type' : str,
-                    'help' : 'filename of wallpaper'
+                (("filename"), {
+                    "type" : str,
+                    "help" : "filename of wallpaper"
                 }),
-                (('-r', '--rect'), {
-                    'type' : str,
-                    'nargs' : 2,
-                    'metavar' : ('WIDTH', 'HEIGHT'),
-                    'default' : [1920,1080],
-                    'help' : 'set width and height of wallpaper'
+                (("-r", "--rect"), {
+                    "type" : str,
+                    "nargs" : 2,
+                    "metavar" : ("WIDTH", "HEIGHT"),
+                    "default" : [1920, 1080],
+                    "help" : "set width and height of wallpaper"
                 }),
-                (('-c', '--color'), {
-                    'type' : str,
-                    'default' : '#1E1E2E',
-                    'help' : 'set color of wallpaper background'
+                (("-c", "--color"), {
+                    "type" : str,
+                    "default" : "#1E1E2E",
+                    "help" : "set color of wallpaper background"
                 }),
-                (('-waifu', '--waifu'), {
-                    'type' : str,
-                    'metavar' : 'PATH',
-                    'default' : './.default/satori.png',
-                    'help' : 'path of waifu'
+                (("-waifu", "--waifu"), {
+                    "type" : str,
+                    "metavar" : "PATH",
+                    "default" : "./.default/satori.png",
+                    "help" : "path of waifu"
                 }),
-                (('-p', '--position'), {
-                    'type' : str,
-                    'metavar' : 'POS',
-                    'default' : 'bottom-right',
-                    'help' : 'Position of waifu'
+                (("-p", "--position"), {
+                    "type" : str,
+                    "metavar" : "POS",
+                    "default" : "bottom-right",
+                    "help" : "Position of waifu"
                 }),
-                (('-o', '--offset'), {
-                    'type' : int,
-                    'nargs' : 2,
-                    'metavar' : ('X', 'Y'),
-                    'default' : [0,0],
-                    'help' :'set offset of waifu'
+                (("-o", "--offset"), {
+                    "type" : int,
+                    "nargs" : 2,
+                    "metavar" : ("X", "Y"),
+                    "default" : [0, 0],
+                    "help" :"set offset of waifu"
                 }),
-                (('-a', '--angle'), {
-                    'type' : int,
-                    'metavar' : 'ANGLE',
-                    'default' : 0,
-                    'help' : 'The angle the waifu should be rotated by'
+                (("-a", "--angle"), {
+                    "type" : int,
+                    "metavar" : "ANGLE",
+                    "default" : 0,
+                    "help" : "The angle the waifu should be rotated by"
                 }),
-                (('-v', '--version'), {
-                    'action' : 'version',
-                    'version' : '{} {}'.format('%(prog)s',self.version),
-                    'help' : 'show version number of %(prog)s'
+                (("-v", "--version"), {
+                    "action" : "version",
+                    "version" : f"%(prog)s {self.version}",
+                    "help" : "show version number of %(prog)s"
                 }),
-                (('-d','--debug'), {
-                    'action' : 'store_true',
-                    'help' : 'show debug information of %(prog)s'
+                (("-d","--debug"), {
+                    "action" : "store_true",
+                    "help" : "show debug information of %(prog)s"
                 }),
-                (('-f','--flip'), {
-                    'action' : 'store_true',
-                    'help' : 'flips waifu image'
+                (("-f","--flip"), {
+                    "action" : "store_true",
+                    "help" : "flips waifu image"
                 }),
-                (('-s', '--show'), {
-                    'action' : 'store_true',
-                    'help' : 'show the wallpaper after it\'s created'
+                (("-s", "--show"), {
+                    "action" : "store_true",
+                    "help" : "show the wallpaper after it\'s created"
                 }),
-                (('--ornament'),{
-                    'type' : str,
-                    'metavar' : 'ORNAMENT',
-                    'default' : './.default/star.png',
-                    'help' : 'ornament'
+                (("--ornament"),{
+                    "type" : str,
+                    "metavar" : "ORNAMENT",
+                    "default" : "./.default/star.png",
+                    "help" : "ornament"
                 }),
-                (('--ornament_num'),{
-                    'type' : int,
-                    'metavar' : 'ORNAMENT_NUM',
-                    'default' : 1000,
-                    'help' : 'ornament'
+                (("--ornament_num"),{
+                    "type" : int,
+                    "metavar" : "ORNAMENT_NUM",
+                    "default" : 1000,
+                    "help" : "ornament"
                 }),
-                (('--ornament_samples'),{
-                    'type' : int,
-                    'metavar' : 'ORNAMENT_SAMPLES',
-                    'default' : 20,
-                    'help' : 'ornament'
+                (("--ornament_samples"),{
+                    "type" : int,
+                    "metavar" : "ORNAMENT_SAMPLES",
+                    "default" : 20,
+                    "help" : "ornament"
                 }),
-                (('--ornament_minimum_distance'),{
-                    'type' : int,
-                    'metavar' : 'ORNAMENT_MINIMUM_DISTANCE',
-                    'default' : 75,
-                    'help' : 'ornament'
+                (("--ornament_minimum_distance"),{
+                    "type" : int,
+                    "metavar" : "ORNAMENT_MINIMUM_DISTANCE",
+                    "default" : 75,
+                    "help" : "ornament"
                 })
             ]
 
-    def parser_wrapper(self, parser, args):
-        if (type(args[0]) == str):
-            parser.add_argument(args[0],**args[1])
-        else:
-            parser.add_argument(*args[0],**args[1])
+    def parser_wrapper(
+        self, 
+        parser: argparse.ArgumentParser, 
+        args: tuple
+        ) -> None:
 
-    def parser_args(self):
-        parser = argparse.ArgumentParser(prog=self.prog_name, description=self.description)
-        for args in self.arguments : self.parser_wrapper(parser,args)
+        if isinstance(args[0], str):
+            parser.add_argument(args[0], **args[1])
+
+        else:
+            parser.add_argument(*args[0], **args[1])
+
+    def parser_args(
+        self
+        ) -> argparse.Namespace:
+
+        parser = argparse.ArgumentParser(
+            prog=self.prog_name, 
+            description=self.description
+        )
+
+        for args in self.arguments:
+            self.parser_wrapper(parser,args)
+
         return parser.parse_args()
--- wallpaper-gen.py
+++ wallpaper-gen.py
@@ -1,172 +1,227 @@
-'''
-     ▄█     █▄     ▄████████  ▄█        ▄█
-    ███     ███   ███    ███ ███       ███
-    ███     ███   ███    ███ ███       ███
-    ███     ███   ███    ███ ███       ███
-    ███     ███ ▀███████████ ███       ███
-    ███     ███   ███    ███ ███       ███
-    ███ ▄█▄ ███   ███    ███ ███▌    ▄ ███▌    ▄
-     ▀███▀███▀    ███    █▀  █████▄▄██ █████▄▄██
-                             ▀         ▀
-       ▄███████▄    ▄████████    ▄███████▄    ▄████████    ▄████████
-      ███    ███   ███    ███   ███    ███   ███    ███   ███    ███
-      ███    ███   ███    ███   ███    ███   ███    █▀    ███    ███
-      ███    ███   ███    ███   ███    ███  ▄███▄▄▄      ▄███▄▄▄▄██▀
-    ▀█████████▀  ▀███████████ ▀█████████▀  ▀▀███▀▀▀     ▀▀███▀▀▀▀▀
-      ███          ███    ███   ███          ███    █▄  ▀███████████
-      ███          ███    ███   ███          ███    ███   ███    ███
-     ▄████▀        ███    █▀   ▄████▀        ██████████   ███    ███
-                                                          ███    ███
-     ▄█     █▄     ▄████████  ▄█     ▄████████ ███    █▄
-    ███     ███   ███    ███ ███    ███    ███ ███    ███
-    ███     ███   ███    ███ ███▌   ███    █▀  ███    ███
-    ███     ███   ███    ███ ███▌  ▄███▄▄▄     ███    ███
-    ███     ███ ▀███████████ ███▌ ▀▀███▀▀▀     ███    ███
-    ███     ███   ███    ███ ███    ███        ███    ███
-    ███ ▄█▄ ███   ███    ███ ███    ███        ███    ███
-     ▀███▀███▀    ███    █▀  █▀     ███        ████████▀
-'''
+"""
+Generate a Waifu on a wallpaper!
+
+Copyright (C) 2023 Momijie
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 
 import math
-import os
 import sys, subprocess
 import random
 import arguments
 
-from PIL import Image, ImageDraw, ImageColor, ImageOps
-
-def rotate_image(angle, image):
-    return image.rotate(angle, expand=True)
-
-def mirror_image(image):
-    return ImageOps.mirror(image)
+from PIL import Image, ImageColor, ImageOps
+from argparse import Namespace
 
 # Just a simple distance formula for 2d points
-def distance(point1, point2):
+def distance(
+    point1: tuple[int, int], 
+    point2: tuple[int, int]
+    ) -> float:
+
     part1 = (point1[0] - point2[0])**2
     part2 = (point1[1] - point2[1])**2
 
     return math.sqrt(part1 + part2)
 
 def points_generation(
-        total_ornaments,
-        sample_size,
-        width,height,
-        minimum_distance = -1,
-        debug=None):
+    total_ornaments: int,
+    sample_size: int,
+    width: int, height: int,
+    minimum_distance: int = -1,
+    ) -> list:
         
     points = []
 
     # Loop through the total number of ornaments
     # we want to create on the wallpaper
-    for i in range(total_ornaments):
+    for _ in range(total_ornaments):
         samples = get_random(sample_size, width, height)
 
         if not points or minimum_distance == -1:
             points.append(samples[0])
+
         else:
-            hold = point_sample_comparison(points, samples, minimum_distance)
+            hold = point_sample_comparison(
+                points, 
+                samples, 
+                minimum_distance
+            )
+
             if len(hold) > 0:
                 points.append(hold[0])
          
     return points
 
-def point_sample_comparison(points,samples,minimum_distance):
-    return [sample for sample in samples if all([distance(
-        point,sample) >= minimum_distance for point in points])]
+def point_sample_comparison(
+    points: list,
+    samples: list | tuple,
+    minimum_distance: int
+    ) -> list:
+
+    return [
+        sample
+        for sample in samples 
+        if all([
+            distance(point, sample) >= minimum_distance 
+            for point in points
+        ])
+    ]
         
 # get a list (or singular point) of random points
 # on a rectangle of defined width and height
-def get_random(sample_size, width, height):
+def get_random(
+    sample_size: int, 
+    width: int, 
+    height: int
+    ) -> tuple | list[tuple]:
 
-    result = (lambda x, width, height :
-        [(
-            int(random.random() * width),
-            int(random.random() * height))
-            for i in range(x)
-        ])(sample_size, width, height)
+    result = [
+        (int(random.random() * width), int(random.random() * height))
+        for _ in range(sample_size)
+    ]
 
     return (result[0][0], result[0][1]) if (len(result) <=1) else result
 
-def add_ornament(args, image):
-    ornament = Image.open(args.ornament, 'r')
-    ornament.convert('RGBA')
+def add_ornament(
+    args: Namespace, 
+    image: Image.Image
+    ) -> None:
 
-    points = points_generation(args.ornament_num,
-            args.ornament_samples,
-            args.rect[0],
-            args.rect[1],
-            args.ornament_minimum_distance,
-            args.debug)
+    ornament = Image.open(args.ornament, "r")
+    ornament.convert("RGBA")
 
-    ornament_width, ornament_height = ornament.size
+    points = points_generation(
+        args.ornament_num,
+        args.ornament_samples,
+        args.rect[0], args.rect[1],
+        args.ornament_minimum_distance
+    )
     
     for point in points:
         image.paste(ornament, point, ornament)
-        
+    
+    ornament.close()
 
-def add_waifu(args, image):
-    waifu = Image.open(args.waifu, 'r')
-    waifu = waifu.convert('RGBA')
-    waifu = rotate_image(args.angle, waifu)
+def add_waifu(
+    args: Namespace, 
+    image: Image.Image
+    ) -> None:
 
-    if(args.flip):
-        waifu = mirror_image(waifu)
+    waifu = Image.open(args.waifu, "r")
+    waifu = waifu.convert("RGBA")
+    waifu = waifu.rotate(args.angle, expand=True)
+
+    if args.flip:
+        waifu = ImageOps.mirror(waifu)
 
     image_width, image_height = image.size
     waifu_width, waifu_height = waifu.size
 
     x_offset, y_offset = args.offset
 
-    if(args.position == 'top-left'):
-        offset = ((0+x_offset), (0+y_offset))
-    elif(args.position == 'bottom-left'):
-        offset = ((0+x_offset), ((image_height-waifu_height)+y_offset))
-    elif(args.position == 'top-right'):
-        offset = (((image_width-waifu_width)+x_offset), (0+y_offset))
-    elif(args.position == 'bottom-right'):
-        offset = (((image_width-waifu_width)+x_offset),
-                 ((image_height-waifu_height)+y_offset))
-    elif(args.position == 'top-center'):
+    if args.position == "top-left":
+        offset = ((0 + x_offset), (0 + y_offset))
+
+    elif args.position == "bottom-left":
+        offset = ((0 + x_offset), ((image_height - waifu_height) + y_offset))
+
+    elif args.position == "top-right":
+        offset = (((image_width - waifu_width) + x_offset), (0 + y_offset))
+
+    elif args.position == "bottom-right":
+        offset = (((image_width - waifu_width) + x_offset),
+                 ((image_height - waifu_height) + y_offset))
+
+    elif args.position == "top-center":
         x_portion = (int(image_width/2) - int(waifu_width/2))
-        offset = ((x_portion+x_offset), (0+y_offset))
-    elif(args.position == 'center'):
+        offset = ((x_portion + x_offset), (0 + y_offset))
+
+    elif args.position == "center":
         x_portion = (int(image_width/2) - int(waifu_width/2))
         y_portion = (int(image_height/2) - int(waifu_height/2))
-        offset = ((x_portion+x_offset), (y_portion+y_offset))
-    elif(args.position == 'bottom-center'):
+        offset = ((x_portion + x_offset), (y_portion + y_offset))
+
+    elif args.position == "bottom-center":
         x_portion = (int(image_width/2) - int(waifu_width/2))
-        offset = ((x_portion+x_offset),
-                 ((image_height-waifu_height)+y_offset))
+        offset = ((x_portion + x_offset),
+                 ((image_height - waifu_height) + y_offset))
+    
+    else:
+        raise SystemError("Invalid position")
 
     image.paste(waifu, offset, waifu)
+    waifu.close()
 
-def base(width, height, color):
-    return Image.new('RGBA', (width, height), color=ImageColor.getrgb(color))
+def show(
+    args: Namespace, 
+    filename: str
+    ) -> None:
 
-def save(image, filename):
-    image.save(filename)
+    if not args.show:
+        return
 
-def show(args, filename):
-    if(args.show):
-        imageViewerCmd = {'linux':'xdg-open',
-                          'win32':'explorer',
-                          'darwin':'open'}[sys.platform]
+    imageViewerCmd = {
+        "linux": "xdg-open",
+        "win32": "explorer",
+        "darwin": "open"
+    }.get(sys.platform)
+    
+    if not imageViewerCmd:
+        raise SystemExit("Couldn't get your platform, exiting")
 
-        subprocess.Popen([imageViewerCmd, filename])
+    subprocess.Popen(
+        [imageViewerCmd, filename]
+    )
 
-def create_wallpaper(args, width, height, color, filename):
-    image = base(width, height, color)
+def create_wallpaper(
+    args: Namespace, 
+    width: int, 
+    height: int, 
+    color: str, 
+    filename: str
+    ) -> None:
+
+    image = Image.new(
+        mode="RGBA", 
+        size=(width, height), 
+        color=ImageColor.getrgb(color)
+    )
+
     add_ornament(args, image)
     add_waifu(args, image)
-    save(image, filename)
+
+    image.save(filename)
 
     show(args, filename)
-def main() -> int:
-    args = arguments.Arguments().parser_args()
-    if(args.debug):
-        print(args)
-    create_wallpaper(args, args.rect[0], args.rect[1], args.color, args.filename)
 
-if __name__ == '__main__':
+def main():
+    args = arguments.Arguments().parser_args()
+
+    if args.debug:
+        print(args)
+    
+    if not args.filename.endswith(".png"):
+        raise SystemError("Filename must end with '.png'")
+
+    create_wallpaper(
+        args, 
+        args.rect[0], args.rect[1], 
+        args.color, 
+        args.filename
+    )
+
+if __name__ == "__main__":
     raise SystemExit(main())
